35fc3f81019a6f48db32ef286f2bf3ed9a9acbf6
harishreddymaryada <maryadareddys@gmail.com>
2017-02-22 09:59:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/a1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  
+});
 app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
   
